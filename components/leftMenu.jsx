19f36cff71798b9a2880f020a22e59ef0c8eb8bd
avinashmishra7 <avinash.mishra@autodesk.com>
2017-09-20 16:16:34 -0700

    CSS changes

--- components/leftMenu.jsx
+++ components/leftMenu.jsx
@@ -22,22 +22,24 @@ const LeftMenu = React.createClass({
 	},
 		
 	render: function() {
+		
+		console.log("selected left menu..."+this.state.selectedLeft);
 		return (
 			<div className="col-xs-2 col-md-2 col-sm-2 col-lg-2 no-pad left_main_menu">
 				<ScrollArea speed={0.8} className="area leftScroll" contentClassName="content" horizontal={false}>
 					<PanelGroup  collapsible defaultExpanded  aria-multiselectable="true" className="col-xs-12 no-pad left_main_accord left_menu_height">
 						
-						<Panel collapsible header="Dashboard"  defaultExpanded className="left_panel IT-Services com_hover dashboard com_menu" onClick={()=>{this.props.leftMenuClick('dashboard')}}></Panel>
+						<Panel collapsible header="Dashboard"  defaultExpanded  className={this.state.selectedLeft=="dashboard"?"select_active left_panel IT-Services com_hover dashboard com_menu":"non_active left_panel IT-Services com_hover dashboard com_menu"} onClick={()=>{this.props.leftMenuClick('dashboard')}}></Panel>
 						
 						<Panel  aria-expanded="true" collapsible defaultExpanded header="Products & Services"  className="left_panel IT-Services com_hover top_fst_menu">
 							<ul className="col-xs-12 no-pad left_panel_ul  ">
-								<li className="col-xs-12 col-sm-12 col-md-12 col-lg-12" onClick={()=>{this.props.leftMenuClick('allProducts')}}>
+								<li className={this.state.selectedLeft=="allProducts"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"}  onClick={()=>{this.props.leftMenuClick('allProducts')}}>
 									<label id="Project_type">All products and services</label>
 								</li>
-								<li className="col-xs-12 col-sm-12 col-md-12 col-lg-12" onClick={()=>{this.props.leftMenuClick('productUpdates')}}>
+								<li className={this.state.selectedLeft=="productUpdates"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"}  onClick={()=>{this.props.leftMenuClick('productUpdates')}}>
 									<label  id="Geography">Product updates</label>
 								</li>
-								<li className="col-xs-12 col-sm-12 col-md-12 col-lg-12" onClick={()=>{this.props.leftMenuClick('trials')}}>
+								<li className={this.state.selectedLeft=="trials"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"} onClick={()=>{this.props.leftMenuClick('trials')}}>
 									<label  id="Geography">Trials</label>
 								</li>
 							</ul>
@@ -45,10 +47,10 @@ const LeftMenu = React.createClass({
 						
 						<Panel  aria-expanded="true" collapsible header="Manage Access" defaultExpanded className="left_panel tablemain-cls com_hover manage">
 							<ul className="col-xs-12 no-pad left_panel_ul" >
-								<li className="col-xs-12"  onClick={()=>{this.props.leftMenuClick('Users')}}>
+								<li className={this.state.selectedLeft=="Users"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"}  onClick={()=>{this.props.leftMenuClick('Users')}}>
 									<label className="col-xs-12 no-pad" htmlFor="Project_Name">Users</label>
 								</li>
-								<li className="col-xs-12" onClick={()=>{this.props.leftMenuClick('Users')}}>
+								<li className={this.state.selectedLeft=="Products"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"} onClick={()=>{this.props.leftMenuClick('Products')}}>
 									<label className="col-xs-12 no-pad" htmlFor="Project_Name">Products</label>
 								</li>
 							</ul>
@@ -56,16 +58,16 @@ const LeftMenu = React.createClass({
 						
 						<Panel  aria-expanded="true" collapsible header="Billing & Orders" defaultExpanded className="left_panel IT-Services com_hover billing">
 							<ul className="col-xs-12 no-pad left_panel_ul  ">
-								<li className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+								<li  className={this.state.selectedLeft=="subscriptions"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"} onClick={()=>{this.props.leftMenuClick('subscriptions')}}>
 									<label id="Project_type">Subscriptions</label>
 								</li>
-								<li className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+								<li  className={this.state.selectedLeft=="upcoming_payments"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"} onClick={()=>{this.props.leftMenuClick('upcoming_payments')}}>
 									<label  id="Geography">Upcoming Payments</label>
 								</li>
-								<li className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+								<li  className={this.state.selectedLeft=="order_history"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"} onClick={()=>{this.props.leftMenuClick('order_history')}}>
 									<label  id="Geography">Order history</label>
 								</li>
-								<li className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+								<li  className={this.state.selectedLeft=="contracts"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"} onClick={()=>{this.props.leftMenuClick('contracts')}}>
 									<label  id="Geography">Contracts</label>
 								</li>
 							</ul>
@@ -73,20 +75,21 @@ const LeftMenu = React.createClass({
 						
 						<Panel  aria-expanded="true" collapsible header="Reporting" defaultExpanded className="left_panel IT-Services com_hover reporting">
 							<ul className="col-xs-12 no-pad left_panel_ul  ">
-								<li className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+								<li className={this.state.selectedLeft=="cloud_service"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"} onClick={()=>{this.props.leftMenuClick('cloud_service')}}>
 									<label id="Project_type">Cloud service usege</label>
 								</li>
 							</ul>
 						</Panel>
 						
-						<Panel  aria-expanded="true" 	collapsible header="Devices" defaultExpanded className="left_panel IT-Services com_hover device_menu">
+						<Panel  aria-expanded="true" collapsible header="Devices" defaultExpanded className="left_panel IT-Services com_hover device_menu">
 							<ul className="col-xs-12 no-pad left_panel_ul  ">
-								<li className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+								<li className={this.state.selectedLeft=="manage_devices"?"select_active col-xs-12 col-sm-12 col-md-12 col-lg-12":"non_active col-xs-12 col-sm-12 col-md-12 col-lg-12"} onClick={()=>{this.props.leftMenuClick('manage_devices')}}>
 									<label id="Project_type">Manage devices</label>
 								</li>
 							</ul>
 						</Panel>
 						<div className="separator"></div>
+						
 						<Panel header="Support" collapsible defaultExpanded className="left_panel IT-Services top_last_menues com_menu"></Panel>
 						<Panel header="Send feedback"  defaultExpanded collapsible  className="left_panel IT-Services top_last_menues com_menu"></Panel>
 						<Panel header="Contact us" defaultExpanded collapsible  className="left_panel IT-Services top_last_menues com_menu"></Panel>
@@ -96,8 +99,9 @@ const LeftMenu = React.createClass({
 						<Panel defaultExpanded header="Legal Notices & Trademarks" collapsible className="left_panel IT-Services top_last_menues com_menu"></Panel>
 						
 						
-						 <Panel header="Quick Link" collapsible defaultExpanded className="left_panel IT-Services com_hover  link_bot" ></Panel> */}
+						 <Panel header="Quicl Link" collapsible defaultExpanded className="left_panel IT-Services com_hover  link_bot" ></Panel> */}
 						<div className="separator"></div>
+						
 					</PanelGroup>
 				</ScrollArea>
 				
