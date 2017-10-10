cfd4d68f2b6cc26a0b7506f91c5f15ff13a466eb
avinashmishra7 <avinash.mishra@autodesk.com>
2017-10-10 13:29:02 -0700

    more updates todo list

--- components/dashBoard.jsx
+++ components/dashBoard.jsx
@@ -8,19 +8,19 @@ const DashBoard = React.createClass({
 	
 	getInitialState: function(){
 		return {
+			index: 0,
+		direction: null,
 		}
+	},
+	
+	handleSelect(selectedIndex, e) {
+		this.setState({
+			index: selectedIndex,
+			direction: e.direction
+		});
 	},
 		
 	render: function() {
-		
-		const data = [
-            {label: "Facebook", value: 100, color: "#3b5998"},
-            {label: "Twitter", value: 60, color: "#00aced"},
-            {label: "Google Plus", value: 30, color: "#dd4b39"},
-            {label: "Pinterest", value: 20, color: "#cb2027"},
-            {label: "Linked In", value: 10, color: "#007bb6"},
-        ]
-		const {expandedSector} = this.state
 		
 		return (
 		<div className="col-xs-12 col-md-12 col-sm-12 col-lg-12 fix_width">
@@ -68,27 +68,31 @@ const DashBoard = React.createClass({
 			<div className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad main_content1">
 				<div className="col-xs-12 col-md-9 col-sm-12 col-lg-9 no-pad">
 					<ul className="col-xs-12 col-md-12 col-sm-12 col-lg-12 com_pad_top btn_grp desktop_view">
-						<li className="col-xs-12 col-md-4 col-sm-4 col-lg-4 no-pad ">
-							<div className="revit_update"></div>
-							<div className="button_upper_text">2 new revit updates avilable</div>
-							<div className="btn_div"><input type="button" name="asas" className="btn_desig btn_download" value="Download now"/></div>
-							<div className="btn_seperator"></div>
-						</li>
-						<li className="col-xs-12 col-md-4 col-sm-4 col-lg-4 no-pad ">
-							<div className="auto_cad"></div>
-							<div className="button_upper_text">AutoCAD is expiring on <br/> October 21,2017</div>
-							<div className="btn_div"><input type="button" name="asas" className="btn_desig btn_renew" value="Renew now"/></div>
-							<div className="btn_seperator"></div>
-						</li>
-						<li className="col-xs-12 col-md-4 col-sm-4 col-lg-4 no-pad ">
-							<div className="auto_desk"></div>
-							<div className="button_upper_text">Get to know your Autodesk <br/>Account</div>
-							<div className="btn_div"><input type="button" name="asas" className="btn_desig btn_tour" value="Take a tour"/></div>
-						</li>
+						
+						<div className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad div_hov">
+							<li className="col-xs-12 col-md-4 col-sm-4 col-lg-4 no-pad ">
+								<div className="revit_update"></div>
+								<div className="button_upper_text">2 new revit updates avilable</div>
+								<div className="btn_div"><input type="button" name="asas" className="btn_desig btn_download" value="Download now"/></div>
+								<div className="btn_seperator"></div>
+							</li>
+							<li className="col-xs-12 col-md-4 col-sm-4 col-lg-4 no-pad ">
+								<div className="auto_cad"></div>
+								<div className="button_upper_text">AutoCAD is expiring on <br/> October 21,2017</div>
+								<div className="btn_div"><input type="button" name="asas" className="btn_desig btn_renew" value="Renew now"/></div>
+								<div className="btn_seperator"></div>
+							</li>
+							<li className="col-xs-12 col-md-4 col-sm-4 col-lg-4 no-pad ">
+								<div className="auto_desk"></div>
+								<div className="button_upper_text">Get to know your Autodesk <br/>Account</div>
+								<div className="btn_div"><input type="button" name="asas" className="btn_desig btn_tour" value="Take a tour"/></div>
+							</li>
+						</div>
 					</ul>
 					
 					{/*Carousel start here*/}
-					<Carousel className="col-xs-12 col-md-12 col-sm-12 col-lg-12 com_pad_top slider">
+					<Carousel className="col-xs-12 col-md-12 col-sm-12 col-lg-12 com_pad_top slider" 
+					 activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect} >
 					
 						<Carousel.Item className="col-xs-12 col-md-12 col-sm-12 col-lg-12 com_pad_top">
 							<div className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad ">
@@ -113,19 +117,6 @@ const DashBoard = React.createClass({
 								<div className="btn_div"><input type="button" name="asas" className="btn_desig btn_tour" value="TAKE A TOUR"/></div>
 							</div>
 						</Carousel.Item>
-					
-					{/* <Carousel.Item>
-						  <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
-						</Carousel.Item>
-						
-						<Carousel.Item>
-						  <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
-						</Carousel.Item>
-						
-						<Carousel.Item>
-						  <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
-					</Carousel.Item> */}
-					
 					</Carousel>
 					{/*Carousel end here*/}
 					
@@ -133,20 +124,20 @@ const DashBoard = React.createClass({
 						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad new_header">
 							<h2>What's new</h2>
 						</li>
-						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad ">
-						Download preference now available under global setting
+						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad sub_li sub_li1">
+							Download preference now available under global setting
 						</li>
-						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad ">
-						New account feature - Download preference
+						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad sub_li">
+							New account feature - Download preference
 						</li>
-						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad ">
-						Learn more about new improvement to subscription management
+						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad sub_li">
+							Learn more about new improvement to subscription management
 						</li>
-						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad ">
-						New release of Revit available, download now
+						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad sub_li">
+							New release of Revit available, download now
 						</li>
-						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad ">
-						Your subscrition of Navisworks(3 seats) expired on December 1 2017
+						<li className="col-xs-12 col-md-12 col-sm-12 col-lg-12 no-pad sub_li">
+							Your subscrition of Navisworks(3 seats) expired on December 1 2017
 						</li>
 					</ul>
 				</div>
@@ -154,47 +145,20 @@ const DashBoard = React.createClass({
 				<div className="col-xs-12 col-md-3 col-sm-12 col-lg-3 no-pad top_images1">
 					<ul className="col-xs-12 col-md-12 col-sm-6 col-lg-12 support_box com_box">
 						<li className="new_header"><h2>Support</h2></li>
-						<li className="sub_li">My Support</li>
+						<li className="sub_li sub_li2">My Support</li>
 						<li className="sub_li">Open support case(2)</li>
 						<li className="sub_li">Contact support</li>
 						<li className="sub_li sub_li_bottom">Access commmunity and user forums</li>
 					</ul>
 					<ul className="col-xs-12 col-md-12 col-sm-6 col-lg-12 feedback_box com_box">
 						<li className="new_header"><h2>Feedback</h2></li>
-						<li className="sub_li">Send feedback</li>
+						<li className="sub_li sub_li2">Send feedback</li>
 						<li className="sub_li">Take a quick survey</li>
 						<li className="sub_li sub_li_bottom">Join an upcoming research study</li>
 					</ul>	
 				</div>
 				
 			</div>
-
-			 {/* <div className="col-xs-12 col-md-6 col-sm-12 col-lg-6 no-pad">
-                <PieChart
-                    data={ data }
-                    expandedSector={expandedSector}
-                    onSectorHover={this.handleMouseEnterOnSector}
-                    sectorStrokeWidth={2}
-                    expandOnHover
-                    shrinkOnTouchEnd
-					className="col-xs-12 col-md-9 col-sm-12 col-lg-9 no-pad"
-                />
-                <div className="col-xs-12 col-md-3 col-sm-12 col-lg-3 no-pad chart_details">
-                {
-                    data.map((element, i) => (
-                        <div key={i}>
-                            <span style={{background: element.color}}></span>
-                            <span style={{fontWeight: this.state.expandedSector === i ? "bold" : null}}>
-                                {element.label} : {element.value} : 
-								<span className="color_details"></span>
-								
-								
-                            </span>
-                        </div>
-                    ))
-                }
-                </div>
-            </div> */}
 			
 		</div>	
 		
