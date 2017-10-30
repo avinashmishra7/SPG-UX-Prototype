79d068180ca3fe742d62f607f5471d01ce241605
avinashmishra7 <avinash.mishra@autodesk.com>
2017-10-30 10:43:21 -0700

    HEADER icons button

--- components/header.jsx
+++ components/header.jsx
@@ -6,9 +6,67 @@ import { Router, Route, Link, browserHistory, History ,RouterContext} from 'reac
 import LeftMenu from './leftMenu.jsx';
 
 const Header = React.createClass({
-		
+	
+	getInitialState: function(){
+		return {
+			active :false,
+			notificationContent:false,
+			helpContent:false,
+		}
+	},
+	
+	toggleClass() {
+		if(!this.state.active){
+			// attach/remove event handler
+			document.addEventListener('click', this.handleOutsideClick, false);
+		} else {
+			document.removeEventListener('click', this.handleOutsideClick, false);
+		}
+		 this.setState({ active: !this.state.active });
+	}, 
+	
+	toggleNotification() {
+		if(!this.state.notificationContent){
+			// attach/remove event handler
+			document.addEventListener('click', this.handleOutsideClick1, false);
+		} else {
+			document.removeEventListener('click', this.handleOutsideClick1, false);
+		}
+        this.setState({ notificationContent: !this.state.notificationContent });
+    },
+	toggleHelp() {
+		if(!this.state.helpContent){
+			// attach/remove event handler
+			document.addEventListener('click', this.handleOutsideClick2, false);
+		} else {
+			document.removeEventListener('click', this.handleOutsideClick2, false);
+		}
+        this.setState({ helpContent: !this.state.helpContent });
+    },
+	handleOutsideClick(e) {
+	// ignore clicks on the component itself
+		if (this.node.contains(e.target)) {
+		return;
+		}
+		this.toggleClass();
+	},
+	handleOutsideClick1(e) {
+	// ignore clicks on the component itself
+		if (this.node.contains(e.target)) {
+		return;
+		}
+		this.toggleNotification();
+	},
+	handleOutsideClick2(e) {
+	// ignore clicks on the component itself
+		if (this.node.contains(e.target)) {
+		return;
+		}
+		this.toggleHelp();
+	},
+
+	
 	render: function() {
-		console.log("header come......",this.props.mobileDashboardClick);
 		return (
 			<div className="top_header">
 				<div className="header">
@@ -21,14 +79,33 @@ const Header = React.createClass({
 									<a className={this.props.mobileDashboardClick?"mobile_close":"mobile_view"} href="#" onClick={this.props.mobileDashboard}></a>
 									<a className="mobile_logo" href="#"></a>
 								</li>
-								<li className="user"><span className="devide"></span><span className="avatar"></span></li>
+								<li className="user">
+									<span className="devide"></span>
+									<span className="avatar" onClick={this.toggleClass} ></span>									
+								</li>
 								<li className="com_icon">
-									<span className="notification"></span>
-									<span className="question"></span>
+									<span className="notification" onClick={this.toggleNotification}></span>
+									<span className="question" onClick={this.toggleHelp}></span>
 								</li>
 							</ul>
 						</nav>
 					</header>
+					{this.state.active ?
+					<div className="com_cont notice" ref={node => { this.node = node; }}>
+						<img src="images/Profile-dropdown.png" className="const_img img_responsive" />
+					</div> : null}
+					
+					{this.state.notificationContent ?
+					<div className="com_cont notice2"  ref={node => { this.node = node; }}>
+						<img src="images/Notification-dropdown.png" className="const_img img_responsive" />
+					</div> : null}
+					
+					{this.state.helpContent ?
+					<div className="com_cont notice1"  ref={node => { this.node = node; }}>
+						<img src="images/Help-drpdown.png" className="const_img img_responsive" />
+					</div> : null}
+					
+					
 				</div>
 			</div>
 		)
